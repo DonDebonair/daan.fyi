656bfbcfabb3b0b4b9ab831efb16a922a1bef90d
Daan Debie <daan@dv.email>
2022-11-07 23:38:53 +0100

    feat: add Mastodon link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Divider, Flex, Text, HStack, IconButton, Link } from '@chakra-ui/react';
 import React, { ReactElement } from 'react';
 import { GrTwitter, GrGithub, GrLinkedin } from 'react-icons/gr';
+import { BsMastodon } from 'react-icons/bs';
 import { DefaultLink } from '@/components/CustomLink';
 import { copyright } from '@/lib/config';
 
@@ -8,6 +9,14 @@ const Footer = (): ReactElement => (
     <Flex direction="column" maxW="800px" w="100%" m="0 auto">
         <Divider mb={4} />
         <HStack justify="center">
+            <Link
+                href="https://fosstodon.org/@daan"
+                title="Mastodon"
+                rel="me noopener noreferrer"
+                isExternal
+            >
+                <IconButton aria-label="Mastodon" icon={<BsMastodon />} variant="ghost" />
+            </Link>
             <Link
                 href="https://twitter.com/DaanDebie"
                 title="Twitter"
